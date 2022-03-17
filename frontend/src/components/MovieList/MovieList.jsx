5da72a7acae188851cc4b5d19e5c42fe17fc5501
Bryce Payne <bryce.payne.97@gmail.com>
2022-03-17 11:15:56 -0500

    got modal to display information

--- frontend/src/components/MovieList/MovieList.jsx
+++ frontend/src/components/MovieList/MovieList.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './MovieList.css'
-import MovieInfo from "../../components/MovieInfo/MovieInfo";
+
 import { MDBBtn,
     MDBModal,
     MDBModalDialog,
@@ -15,38 +15,77 @@ import { MDBBtn,
 const MovieList = (props) => {
 
     const [centredModal, setCentredModal] = useState(false);
+    const[movieSelected, setSelectedMovieBool] = useState(false)
     const [modalData, setModalData] = useState(null);
-    // const toggleShow = () => setCentredModal(!centredModal);
+    const toggleShow = () => setCentredModal(!centredModal);
 
     console.log(modalData); // shows data of selected movie
 
     function toggleModal() {
+        
         setCentredModal(!centredModal);
         // toggleShow();
         console.log(modalData);
     }
 
     function setData(movie) {
-        setModalData(movie)
+        setModalData(movie);
+        setCentredModal(movie);
+        setSelectedMovieBool(true)
+        
     }
+
+//    if(word == "Yes"){
+
+//    }
+
+//    word=="yes" ? console.log(true) : console.log(false)
+    
 
     return (  
         <div className="container-fluid movie-list">
+       
                 <div className="row">
 
                     {props.movies.slice(0, 10).map((movie, index) => (
                             
-                            <img onClick={() => {setData(movie); setCentredModal(!centredModal);}} key={index} className="movie-poster" src={movie.image} alt='movie'></img>
+                            <img onClick={() => setData(movie)} key={index} className="movie-poster" src={movie.image} alt='movie'></img>
                             
                             
                     ))}
+                    {}
                     <>
+                    {movieSelected?
                             <MDBModal tabIndex='-1' show={centredModal} setShow={setCentredModal}>
-                            <MDBModalDialog centered>
+                            <MDBModalDialog centered size='lg'>
                                 <MDBModalContent>
-                                    <MDBModalHeader>
-                                        <MDBModalTitle></MDBModalTitle>
-                                        <MDBBtn className='btn-close' color='none' onClick={toggleModal}></MDBBtn>
+                                <MDBModalHeader>
+                                    <MDBModalTitle>Grids in modals</MDBModalTitle>
+                                    <MDBBtn
+                                        type='button'
+                                        className='btn-close'
+                                        color='none'
+                                        onClick={toggleShow}
+                                    ></MDBBtn>
+                                    </MDBModalHeader>
+                                    <MDBModalBody>
+                                    <div className='container-fluid bd-example-row'>
+                                        <div className='row'>
+                                            <div className='col-md-4' ><img style={{width: 300, height: 450}} src={modalData.image} alt="" /></div>
+                                            <div className='col-md-4 ms-auto'><h2>{modalData.title}</h2></div>
+                                        </div>
+                                        <div className='col-md-2'>{modalData.plot}</div>
+                                    </div>
+                                    </MDBModalBody>
+                                    <MDBModalFooter>
+                                    <MDBBtn color='secondary' onClick={toggleShow}>
+                                        Close
+                                    </MDBBtn>
+                                    <MDBBtn>Save changes</MDBBtn>
+                                    </MDBModalFooter>
+                                    {/* <MDBModalHeader>
+                                        <MDBModalTitle>{modalData.title}</MDBModalTitle>
+                                        <MDBBtn className='btn-close' color='none' onClick={toggleModal}>????</MDBBtn>
                                     </MDBModalHeader>
                                     <MDBModalBody>
                                         <p>
@@ -57,10 +96,11 @@ const MovieList = (props) => {
                                             Close
                                         </MDBBtn>
                                         <MDBBtn>Save changes</MDBBtn>
-                                    </MDBModalFooter>
+                                    </MDBModalFooter> */}
                                 </MDBModalContent>
                             </MDBModalDialog>
                         </MDBModal>
+                        :null}
                         </>
             </div>
         </div>
